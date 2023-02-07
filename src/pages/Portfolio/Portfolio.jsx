aa83e69a05610644331b38ff2bcb8b74f9c4466a
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-02-07 12:30:24 +0500

    test

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 const Portfolio = () => {
- const [isDay, setIsDay] = useState(true)
+  const [isDay, setIsDay] = useState(true)
 
- const toggleNightToDay = () => {
-   setIsDay(!isDay)
- }
+  const toggleNightToDay = () => {
+    setIsDay(!isDay)
+  }
 
- return (
-   <div style={{ backgroundColor: isDay ? '#fff' : '#000', height: '100vh' }}>
-     <button onClick={toggleNightToDay}>Toggle Night/Day</button>
-   </div>
- )
-};
+  return (
+    <div style={{ backgroundColor: isDay ? '#fff' : '#000', height: '100vh' }}>
+      <button onClick={toggleNightToDay}>Toggle Night/Day</button>
+    </div>
+  )
+}
 
-export default Portfolio;
+export default Portfolio
