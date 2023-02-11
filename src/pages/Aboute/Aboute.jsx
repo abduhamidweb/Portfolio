83a9b2484271a3ada47cf84d3a9c1b1e743004ea
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-02-11 23:36:27 +0500

    profil page add grid card for text and img

--- src/pages/Aboute/Aboute.jsx
+++ src/pages/Aboute/Aboute.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 import './Aboute.scss'
+import myimg from "../../assets/images/myphoto.jpg"
+import { Autoplay, Pagination, Navigation } from 'swiper'
+
 const Aboute = () => {
   return (
     <>
-      <div className='about-container'>
-        <h2 className='section-title'>About Me</h2>
-        <p className='about-description'>
-          Hi, I'm a software developer with a passion for creating beautiful and
-          functional websites. I have a strong background in front-end
-          technologies such as React, HTML, CSS, and JavaScript. I also have
-          experience with back-end technologies such as Node.js, Express, and
-          MongoDB.
-        </p>
-        <p className='about-description'>
-          My goal is to always create high-quality, user-friendly web
-          applications that meet the needs of my clients. I enjoy the challenge
-          of taking a complex problem and breaking it down into smaller,
-          manageable tasks in order to find the best solution.
-        </p>
-        <p className='about-description'>
-          When I'm not coding, I love to travel, read, and try new restaurants.
-          I also enjoy playing piano and guitar.
-        </p>
-      </div>
+      <section id='aboute' className='aboute'>
+        <div className='container aboute-container'>
+          <div className='aboute-container__row'>
+            <div className='aboute-container__row__col__1'>
+              <h3 className='aboute-container__row__col__1__title'>Col 1</h3>
+            </div>
+            <div className='aboute-container__row__col__2'>
+              <h3 className='aboute-container__row__col__2__title'>Col 2</h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
