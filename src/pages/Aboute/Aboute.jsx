528892437b8153c0c3065b14666250c0e807eb36
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-02-14 19:58:14 +0500

    link add aboute

--- src/pages/Aboute/Aboute.jsx
+++ src/pages/Aboute/Aboute.jsx
@@ -1,12 +1,31 @@
 import React, { useRef, useEffect } from 'react'
 import './Aboute.scss'
-import myimg from '../../assets/images/myphoto.jpg'
+import myimg from '../../assets/images/person.png'
 import Typed from 'typed.js'
 import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper'
+// Animation
+import { fadeIn } from 'react-animations'
+import { bounce } from 'react-animations'
+import Radium, { StyleRoot } from 'radium'
+
 const Aboute = () => {
   const el = useRef(null)
   const typed = useRef(null)
+  // const progressCircle = useRef(null)
+  const progressContent = useRef(null)
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    // progressCircle.current.style.setProperty('--progress', 1 - progress)
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`
+  }
   const colors = ['#3498db', '#2ecc71', '#e74c3c', '#9b59b6', '#f1c40f']
+
   useEffect(() => {
     const options = {
       strings: [
@@ -35,6 +54,8 @@ const Aboute = () => {
       typed.current.destroy()
     }
   }, [])
+   
+  
   return (
     <>
       <section id='aboute' className='aboute'>
@@ -42,6 +63,7 @@ const Aboute = () => {
           <div className='aboute-container__row'>
             <div className='aboute-container__row__col__1'>
               <h2 className='hello'>Hello!! 👋</h2>
+
               <p className='name'>
                 {' '}
                 My name is Abduhamid, and I am a web developer.
@@ -53,19 +75,31 @@ const Aboute = () => {
               </p>
               <p className='hobbiy'>
                 When I'm not coding, you can find me
-                <a href='#' className='ms-2'>
+                <a href='https://t.me/AbduhamidBotirov' target='_blank' className='ms-2'>
                   Telegram
                 </a>
-                <a href='#' className='ms-2'>
+                <a
+                  href='https://www.instagram.com/code_snape_photo/'
+                  target='_blank'
+                  className='ms-2'
+                >
                   Instagram
                 </a>
                 <a href='#' className='ms-2'>
                   Facebook
                 </a>
-                <a href='#' className='ms-2'>
+                <a
+                  href='https://www.linkedin.com/in/abduhamid-botirov-91a44b253/'
+                  target='_blank'
+                  className='ms-2'
+                >
                   Linkeden
                 </a>
-                <a href='#' className='ms-2'>
+                <a
+                  href='https://github.com/abduhamidweb'
+                  target='_blank'
+                  className='ms-2'
+                >
                   Github
                 </a>
                 .
@@ -73,7 +107,7 @@ const Aboute = () => {
               <p className='links'>
                 Feel free to take a look at my <a href='/projects'>projects</a>{' '}
                 to see some of my recent work, or{' '}
-                <a href='/contact'>contact me</a> if you'd like to work
+                <Link to='/contact'>contact me</Link> if you'd like to work
                 together.
               </p>
               <div className='portfolioOrResume mt-4 mb-4'>
@@ -86,7 +120,55 @@ const Aboute = () => {
               </div>
             </div>
             <div className='aboute-container__row__col__2'>
-              <h3 className='aboute-container__row__col__2__title'>Col 2</h3>
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                onAutoplayTimeLeft={onAutoplayTimeLeft}
+                className='mySwiper'
+              >
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className='img-wrapper'>
+                    <img src={myimg} alt='img' className='img-fluid' />
+                  </div>
+                </SwiperSlide>
+                <div className='autoplay-progress' slot='container-end'>
+                  <span ref={progressContent}></span>
+                </div>
+              </Swiper>
             </div>
           </div>
         </div>
