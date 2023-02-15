11a41939502347adc6b5da45b0c5bbee961d97ce
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-02-15 13:55:44 +0500

    structure

--- src/pages/Portfolio/DetailPage.jsx
+++ src/pages/Portfolio/DetailPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React from 'react'
+import './detail.scss'
 const DetailPage = () => {
-    return (
-        <>
-         <h2>Detail</h2>   
-        </>
-    );
-};
+  return (
+    <>
+      <section className='detail'>
+        <div className='container'>
+          <div className='detail__content__wrap'>
+            <div className='detail__content__col col-1__content'>ss</div>
+            <div className='detail__content__col col-2__content'>ss</div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
 
-export default DetailPage;
+export default DetailPage
