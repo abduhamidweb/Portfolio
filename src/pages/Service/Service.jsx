c518685390e221c13ab016c1346f55332234517f
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-02 10:27:25 +0500

    oke

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import "./service.scss"
+import React from 'react'
+import './service.scss'
 const Service = () => {
-    return (
-      <>
-        <section id='services'>
+  return (
+    <>
+      {/* <section id='services'>
           <div className='container'>
             <h2 className='section-title'>Our Services</h2>
             <div className='services-list'>
@@ -39,9 +39,110 @@ const Service = () => {
               </div>
             </div>
           </div>
-        </section>
-      </>
-    )
-};
+        </section> */}
+
+      <section id='services' class='services'>
+        <div class='container'>
+          <div class='section-title'>
+            <h2>Services</h2>
+            <p>
+              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+              Quia fugiat sit in iste officiis commodi quidem hic quas.
+            </p>
+          </div>
+
+          <div class='row'>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 col-sm-12 p-2'>
+              <div class=' icon-box p-4' data-aos='fade-up'>
+                <div class='icon'>
+                  <i class='bi bi-briefcase'></i>
+                </div>
+                <h4 class='title'>
+                  <a href=''>Lorem Ipsum</a>
+                </h4>
+                <p class='description'>
+                  Voluptatum deleniti atque corrupti quos dolores et quas
+                  molestias excepturi sint occaecati cupiditate non provident
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
 
 export default Service
